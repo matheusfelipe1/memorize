a93ea1096cb0ba88535df70403dd1e1eb35a69d6
Matheus Felipe Silva Santos <68447982+matheusfelipe1@users.noreply.github.com>
2024-04-17 20:59:28 -0300

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Matheus Felipe on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
